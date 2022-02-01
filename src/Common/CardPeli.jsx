9d754bf555b69d2c4655ce42540efda2f5fcd9bd
Leandro Heliz <leandroheliz@gmail.com>
2022-01-31 23:40:02 -0300

    cards

--- src/Common/CardPeli.jsx
+++ src/Common/CardPeli.jsx
@@ -8,25 +8,32 @@ const CardPeli = () => {
   useEffect(()=>{
     Axios.get("https://api.themoviedb.org/3/movie/popular?api_key=fc707b8b5314976d690dbe67fbe2defe")
     .then(function(response) {
-      
           const peliculas = response.data.results.map((pelisArray,indice) => pelisArray =
-          <div className="d-flex card mb-3 mx-auto" key = {indice} style={{width: '18rem'}}>
+          <div className="col">
+          <div className=" card mb-3 mx-auto" key = {indice} style={{width: '18rem'}}>
             <div className="card-body">
-            <img src={'https://image.tmdb.org/t/p/w500' + pelisArray.poster_path} className="card-img" alt=""></img>
+            <img src={'https://image.tmdb.org/t/p/w500' + pelisArray.poster_path} className="card-img" alt=""/>
+              <div className="d-flex justify-content-evenly">
               <h5 className="card-title text-center">{pelisArray.title}</h5>
               <p className="card-text">{pelisArray.vote_average}⭐️</p>  
-              <button className="btn btn-dark" >Ver Más</button>
+              </div>
+                {/* {pelisArray.overview} */}
+                <p>Fecha de estreno: {pelisArray.release_date}</p>
+                </div>
+              <button className="btn btn-dark">Ver Pelicula</button>
             </div>
-          </div> )
-        console.log(response.data.results)
+          </div>)
         setPelis(peliculas)
-      
       })
     },[])
 return (
   <div className="row">
-    <h1 className="text-center mx-auto p-2">PELICULAS POPULARES</h1>
+    <h3 className="text-center mx-auto p-2 text-white">Peliculas Populares</h3>
+    <div className="container">
+      <div className="row">
     {pelis}
+      </div>
+    </div>
   </div>
 );
 }
--- src/Common/Navbar.jsx
+++ src/Common/Navbar.jsx
@@ -2,20 +2,21 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <div className="container-fluid">
-    <span className="navbar-brand">Mentlify App</span>
+    <span className="navbar-brand text-white">Mentlify App</span>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-       <NavLink to="/" className="nav-link">inicio</NavLink>
-       <NavLink to="/" className="nav-link">Estrenos</NavLink>
+       <NavLink to="/" className="nav-link text-white">inicio</NavLink>
+       <NavLink to="/" className="nav-link text-white">Estrenos</NavLink>
+       <NavLink to="/" className="nav-link text-white">Estrenos</NavLink>
       </ul>
       <form className="d-flex">
         <input className="me-2 border-0" type="search" placeholder=" Buscar Pelicula" aria-label="Search" />
-        <button className="boton" type="submit">Buscar</button>
+        <button className="boton text-white" type="submit">Buscar</button>
       </form>
     </div>
   </div>
